86ab6440b438324944bdf87797e891ef28f4ef6b
Maimuna <maimunaibrahim808@gmail.com>
2024-10-10 03:26:57 +0100

    changes

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,48 +1,46 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 const Header = (props) => {
-  console.log(props)
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  console.log(props);
+  return <h1>{props.course}</h1>;
+};
 const Content = (props) => {
-  console.log(props)
+  console.log(props);
   return (
     <div>
-       <Part />
       <Part />
-      <Part/>
+      <Part />
+      <Part />
     </div>
-  )
-}
+  );
+};
 const Total = (props) => {
-  console.log(props)
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
-
-
+  console.log(props);
+  return <div></div>;
+};
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-
+  const course  = {
+  name: "Half Stack application development",
+  parts: [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  
+  ]
+  }
   return (
     <div>
       <h1>{course}</h1>
@@ -58,17 +56,10 @@ const App = () => {
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
 
       <Header course={course} />
-      <Content  />
-      <Total />
+      <Content parts ={parts} />
+      <Total parts={parts}/>
     </div>
+  );
+};
 
-
-      
-    
-  )
-
-  
-}
-  
-
-export default App
+export default App;
