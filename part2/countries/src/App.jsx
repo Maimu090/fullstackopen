fcac175c2b00fef12bb72a6965b91310f43d1dc1
Maimuna <maimunaibrahim808@gmail.com>
2024-11-09 03:17:19 +0100

    new action

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -2,34 +2,62 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Country from './components/Country';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [countries, setCountries] = useState([]);
+	const [showCountry, setShowCountry] = useState({});
+	const [country, setCountry] = useState('');
+	const [filterCountries, setFilterCountries] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	useEffect(() => {
+		axios.get('https://restcountries.eu/rest/v2/all').then((response) => {
+			setCountries(response.data);
+		});
+	}, []);
+
+	useEffect(() => {
+		setShowCountry(
+			filterCountries.length === 1 ? { ...filterCountries[0] } : {}
+		);
+	}, [filterCountries]);
+
+	const searchCountry = (e) => {
+		setCountry(e.target.value);
+		setFilterCountries(
+			countries.filter(
+				(country) =>
+					country.name.toLowerCase().search(e.target.value.toLowerCase()) !== -1
+			)
+		);
+	};
+
+	const showCountries = () => {
+		if (filterCountries.length <= 1) return;
+		return filterCountries.map((country) => (
+			<p key={country.name}>
+				{country.name}
+				<button onClick={() => setShowCountry(country)}>show</button>
+			</p>
+		));
+	};
+
+	return (
+		<>
+			<p>
+				find countries{' '}
+				<input value={country} name='country' onChange={searchCountry} />
+			</p>
+			{filterCountries.length > 10 ? (
+				<p>Too many matches, specify another filter</p>
+			) : (
+				showCountries()
+			)}
+			{showCountry.name && <Country data={showCountry} />}
+		</>
+	);
 }
 
-export default App
+export default App;
