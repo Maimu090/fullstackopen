3c1d1e26483727aa57da5b953dd067d6264846b5
Maimuna <maimunaibrahim808@gmail.com>
2024-11-10 07:00:34 +0100

    new action

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,63 +1,147 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Country from './components/Country';
+import React, { useState, useEffect } from 'react';
+import Content from './components/Content';
+import Filter from './components/Filter';
+import Notification from './components/Notification';
+import Form from './components/Form';
+import apiService from './services/api';
 
-function App() {
-	const [countries, setCountries] = useState([]);
-	const [showCountry, setShowCountry] = useState({});
-	const [country, setCountry] = useState('');
-	const [filterCountries, setFilterCountries] = useState([]);
+const App = () => {
+	const [persons, setPersons] = useState([]);
+	const [message, setMessage] = useState(null);
+	const [newNumber, setNewNumber] = useState('');
+	const [allPersons, setAllPersons] = useState([]);
+	const [newFilter, setNewFilter] = useState('');
+	const [newName, setNewName] = useState('');
 
 	useEffect(() => {
-		axios.get('https://restcountries.eu/rest/v2/all').then((response) => {
-			setCountries(response.data);
+		apiService.getAll().then((initialPersons) => {
+			setAllPersons(initialPersons);
 		});
 	}, []);
 
-	useEffect(() => {
-		setShowCountry(
-			filterCountries.length === 1 ? { ...filterCountries[0] } : {}
-		);
-	}, [filterCountries]);
+	const addPerson = (event) => {
+		event.preventDefault();
+		const person = allPersons.filter((person) => person.name === newName);
 
-	const searchCountry = (e) => {
-		setCountry(e.target.value);
-		setFilterCountries(
-			countries.filter(
-				(country) =>
-					country.name.toLowerCase().search(e.target.value.toLowerCase()) !== -1
-			)
-		);
+		const personToAdd = person[0];
+		const updatedPerson = { ...personToAdd, number: newNumber };
+
+		if (person.length !== 0) {
+			if (
+				window.confirm(
+					`${personToAdd.name} is already added to the phonebook, replace the old number with a new one ?`
+				)
+			) {
+				apiService
+					.update(updatedPerson.id, updatedPerson)
+					.then((returnedPerson) => {
+						setAllPersons(
+							allPersons.map((personItem) =>
+								personItem.id !== personToAdd.id ? personItem : returnedPerson
+							)
+						);
+						console.log(`${returnedPerson.name} successfully updated`);
+						setNewName('');
+						setNewNumber('');
+						setMessage(`${updatedPerson.name} was successfully updated`);
+						setTimeout(() => {
+							setMessage(null);
+						}, 5000);
+					})
+					.catch((error) => {
+						console.log(error);
+						setAllPersons(
+							allPersons.filter((person) => person.id !== updatedPerson.id)
+						);
+						setNewName('');
+						setNewNumber('');
+						setMessage(
+							`[ERROR] ${updatedPerson.name} was already deleted from server`
+						);
+						setTimeout(() => {
+							setMessage(null);
+						}, 5000);
+					});
+			}
+		} else {
+			const personToAdd = {
+				name: newName,
+				number: newNumber,
+			};
+			apiService
+				.create(personToAdd)
+				.then((returnedPerson) => {
+					setAllPersons(allPersons.concat(returnedPerson));
+					setNewName('');
+					setNewNumber('');
+					setMessage(`${newName} was successfully added`);
+					setTimeout(() => {
+						setMessage(null);
+					}, 5000);
+				})
+				.catch((error) => {
+					setMessage(`[ERROR] ${error.response.data.error}`);
+					setTimeout(() => {
+						setMessage(null);
+					}, 5000);
+					console.log(error.response.data);
+				});
+		}
 	};
 
-	const showCountries = () => {
-		if (filterCountries.length <= 1) return;
-		return filterCountries.map((country) => (
-			<p key={country.name}>
-				{country.name}
-				<button onClick={() => setShowCountry(country)}>show</button>
-			</p>
-		));
+	const deletePerson = (id) => {
+		const filteredPerson = allPersons.filter((person) => person.id === id);
+		const personName = filteredPerson[0].name;
+		const personId = filteredPerson[0].id;
+		if (window.confirm(`Delete ${personName} ?`)) {
+			apiService.remove(personId);
+			console.log(`${personName} successfully deleted`);
+			setMessage(`${personName} was successfully deleted`);
+			setAllPersons(allPersons.filter((person) => person.id !== personId));
+			setTimeout(() => {
+				setMessage(null);
+			}, 5000);
+		}
+	};
+
+	const handleNameChange = (event) => {
+		setNewName(event.target.value);
+	};
+
+	const handleNumberChange = (event) => {
+		setNewNumber(event.target.value);
+	};
+
+	const handleFilterChange = (event) => {
+		setNewFilter(event.target.value);
+		const regex = new RegExp(newFilter, 'i');
+		const filteredPersons = () =>
+			allPersons.filter((person) => person.name.match(regex));
+		setPersons(filteredPersons);
 	};
 
 	return (
-		<>
-			<p>
-				find countries{' '}
-				<input value={country} name='country' onChange={searchCountry} />
-			</p>
-			{filterCountries.length > 10 ? (
-				<p>Too many matches, specify another filter</p>
-			) : (
-				showCountries()
-			)}
-			{showCountry.name && <Country data={showCountry} />}
-		</>
+		<div>
+			<h2>Phonebook</h2>
+			<Notification message={message} />
+			<Filter value={newFilter} onChange={handleFilterChange} />
+			<h2>Add new person</h2>
+			<Form
+				onSubmit={addPerson}
+				newName={newName}
+				handleNameChange={handleNameChange}
+				newNumber={newNumber}
+				handleNumberChange={handleNumberChange}
+			/>
+			<h2>Numbers</h2>
+			<Content
+				persons={persons}
+				allPersons={allPersons}
+				deletePerson={deletePerson}
+			/>
+		</div>
 	);
-}
+};
 
 export default App;
+  
